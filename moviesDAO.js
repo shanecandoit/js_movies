026b906215e7a7260a5b91e1260087274bbb8884
outOfBounds <shanecandoit@gmail.com>
2022-10-08 09:26:49 -0500

    gets movies

--- moviesDAO.js
+++ moviesDAO.js
@@ -1,19 +1,48 @@
-
-let movies
+let movies;
 
 class Movies {
   async injectDB(conn) {
     if (movies) {
-      return
+      // console.log("movies already injected", movies);
+      return;
     }
     try {
-      // client.db("sample_mflix").collection("movies");
-      movies = await conn.db('sample_mflix').collection("movies")
-      console.log('movies', movies);
+      movies = await conn.db("sample_mflix").collection("movies");
+      // console.log("moviesDAO", movies);
     } catch (e) {
       console.error(
-        `Unable to establish a collection handle in moviesDAO: ${e}`,
-      )
+        `Unable to establish a collection handle in moviesDAO: ${e}`
+      );
+    }
+  }
+
+  async getMovies({ filters = null, page = 0, moviesPerPage = 20 } = {}) {
+    console.log("getMovies", filters, page, moviesPerPage);
+    let query;
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("cast" in filters) {
+        query = { cast: { $in: [filters["cast"]] } };
+      } else if ("genre" in filters) {
+        query = { genres: { $in: [filters["genre"]] } };
+      }
+    }
+
+    let cursor;
+    try {
+      cursor = await movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
+
+      const moviesList = await cursor.toArray();
+      const totalNumMovies = await movies.countDocuments(query);
+      console.log(`movies #${moviesList.length} of ${totalNumMovies}`);
+      return { moviesList, totalNumMovies };
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
     }
   }
 }
--- server.js
+++ server.js
@@ -1,52 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
+
 const port = process.env.port || 3000;
 
-const path = require("path");
-const dotenv = require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+const envs = process.env;
+const mongoUrl = envs.MONGO_DB;
+console.log("mongoUrl", mongoUrl);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const envVariables = process.env;
-const mongoUrl = envVariables.MONGO_DB;
-
-
-const Movies = require('./moviesDAO');
+const Movies = require("./moviesDAO");
 const moviesDAO = new Movies();
 // const uri = "mongodb+srv://<username>:<password>@cluster0.g7hbkno.mongodb.net/?retryWrites=true&w=majority";
-console.log('moviesDAO', moviesDAO);
+console.log("moviesDAO", moviesDAO);
 
-console.log('mongoUrl', mongoUrl);
-const client = new MongoClient(mongoUrl, { 
-    useNewUrlParser: true, useUnifiedTopology: true, 
-    serverApi: ServerApiVersion.v1 });
-client.connect();
-const collection = client.db("test").collection("devices");
-console.log('collection', collection);
+async function main() {
+  console.log("mongoUrl", mongoUrl);
+  const client = new MongoClient(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
+  await client.connect();
+  const collection = client.db("test").collection("devices");
+  console.log("collection", collection);
 
+  await moviesDAO.injectDB(client);
+  let movies = await moviesDAO.getMovies();
 
-let movies = moviesDAO.injectDB(client)
+  app.use(express.json());
 
-console.log('movies', movies);
-// perform actions on the collection object
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 
-//client.close();
-
-
-
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-
-app.get('/api/v1/movies', (req, res) => {
+  app.get("/api/v1/movies", (req, res) => {
+    console.log("req", req);
+    console.log("movies", movies);
     res.send(movies);
-});
+  });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Server is running on: http://localhost:${port}`);
+  });
+}
+
+main().catch(console.error);
